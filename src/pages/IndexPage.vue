b6cb8e99a22b9ee1936b1dbbade3010833078d48
Kathy Ems <kathy-ems@users.noreply.github.com>
2022-08-03 06:14:47 -0700

    shows various quasar components (card, dropdown, button)

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,30 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-btn label="click me" color="green"></q-btn>
+    <q-card style="width: 350px">
+      <q-card-section class="bg-blue text-white">
+        My Card
+      </q-card-section>
+      <q-card-section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque aperiam, obcaecati delectus ea quos minus vel vero nobis eaque amet officia illum hic, alias necessitatibus corporis magni excepturi soluta itaque.
+      </q-card-section>
+    </q-card>
+    <q-select outlined label="outlined" :options="['Luke', 'shannen', 'lilly']" v-model="selected"></q-select>
+    <q-select borderless label="borderless" :options="['Luke', 'shannen', 'lilly']" v-model="selected"></q-select>
+    <q-select filled label="filled" :options="['Luke', 'shannen', 'lilly']" v-model="selected"></q-select>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
+// Options API vs Compsition API (later will conver to CompositionAPI)
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      selected: null
+    }
+  }
 })
 </script>
